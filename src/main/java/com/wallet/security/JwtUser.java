9482307be7d9d3e9e55da4b7e162147868003422
Valdinei Reis <valdineimtz@gmail.com>
2021-06-20 19:36:42 -0300

    Configuração do token JWT

---
+++ src/main/java/com/wallet/security/JwtUser.java
@@ -0,0 +1,61 @@
+package com.wallet.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class JwtUser implements UserDetails {
+	
+	private static final long serialVersionUID = -1847115008921214936L;
+	
+	private Long id;
+	private String username;
+	private String password;
+	
+	public JwtUser(Long id, String username, String password) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+}

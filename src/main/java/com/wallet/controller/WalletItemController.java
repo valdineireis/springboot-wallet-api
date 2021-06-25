ae5685e38fdb104375461ec989183f4232d2e7c3
Valdinei Reis <valdineimtz@gmail.com>
2021-06-25 00:56:29 -0300

    Verificação se o usuário logado tem acesso a carteira

--- src/main/java/com/wallet/controller/WalletItemController.java
+++ src/main/java/com/wallet/controller/WalletItemController.java
@@ -26,9 +26,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wallet.dto.WalletItemDTO;
+import com.wallet.entity.UserWallet;
 import com.wallet.entity.WalletItem;
 import com.wallet.response.Response;
+import com.wallet.service.UserWalletService;
 import com.wallet.service.WalletItemService;
+import com.wallet.util.Util;
 import com.wallet.util.enums.TypeEnum;
 
 @RestController
@@ -37,6 +40,9 @@ public class WalletItemController {
 
 	@Autowired
 	private WalletItemService service;
+	
+	@Autowired
+	private UserWalletService userWalletService;
 	
 	@PostMapping
 	public ResponseEntity<Response<WalletItemDTO>> create(
@@ -61,6 +67,14 @@ public class WalletItemController {
 			@RequestParam(name = "page", defaultValue = "0") int page) {
 		
 		Response<Page<WalletItemDTO>> response = new Response<Page<WalletItemDTO>>();
+		
+		Optional<UserWallet> uw = userWalletService.findByUserIdAndWalletId(Util.getAuthenticatedUserId(), wallet);
+		
+		if (! uw.isPresent()) {
+			response.getErrors().add("Você não tem acesso a essa carteira");
+			return ResponseEntity.badRequest().body(response);
+		}
+		
 		Page<WalletItem> items = service.findBetweenDates(wallet, startDate, endDate, page);
 		Page<WalletItemDTO> dto = items.map(i -> new WalletItemDTO(i));
 		response.setData(dto);
---
+++ src/main/java/com/wallet/util/Util.java
@@ -0,0 +1,32 @@
+package com.wallet.util;
+
+import java.util.Optional;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import com.wallet.entity.User;
+import com.wallet.service.UserService;
+
+@Component
+public class Util {
+
+	private static UserService staticService;
+	
+	public Util(UserService service) {
+		Util.staticService = service;
+	}
+	
+	public static Long getAuthenticatedUserId() {
+		try {
+			Optional<User> user = staticService
+					.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
+
+			if (user.isPresent()) {
+				return user.get().getId();
+			}
+		} catch (Exception e) {
+		}
+		return null;
+	}
+}
